58d94a59d96db5311dc93903863ef0c70a539fd2
Can Uludag <cucanuludag@gmail.com>
2018-02-28 18:20:05 +0300

    feat: Bind layout views to Fragments

--- app/src/main/java/com/uludag/can/spacexdroid/home/launches/LaunchesFragment.java
+++ app/src/main/java/com/uludag/can/spacexdroid/home/launches/LaunchesFragment.java
@@ -4,16 +4,47 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.uludag.can.spacexdroid.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class LaunchesFragment extends Fragment {
+
+    @BindView(R.id.recycler_view_launches)
+    RecyclerView mRecyclerViewLaunches;
+    @BindView(R.id.tv_launches_error)
+    TextView mTvLaunchesError;
+    @BindView(R.id.loading_launches)
+    View mLoadingLaunches;
+
+    private Unbinder mUnbinder;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_launches, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mUnbinder != null) {
+            mUnbinder.unbind();
+            mUnbinder = null;
+        }
     }
 }
--- app/src/main/java/com/uludag/can/spacexdroid/home/upcomings/UpcomingsFragment.java
+++ app/src/main/java/com/uludag/can/spacexdroid/home/upcomings/UpcomingsFragment.java
@@ -4,17 +4,47 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.uludag.can.spacexdroid.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class UpcomingsFragment extends Fragment {
+
+    @BindView(R.id.recycler_view_upcomings)
+    RecyclerView mRecyclerViewUpcomings;
+    @BindView(R.id.tv_upcomings_error)
+    TextView mTvUpcomingsError;
+    @BindView(R.id.loading_upcomings)
+    View mLoadingUpcomings;
+
+    private Unbinder mUnbinder;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_upcomings, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mUnbinder != null) {
+            mUnbinder.unbind();
+            mUnbinder = null;
+        }
     }
 }
