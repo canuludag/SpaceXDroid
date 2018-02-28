dee4669f1a9e0674878328a7dbefef21473b3557
Can Uludag <cucanuludag@gmail.com>
2018-02-28 17:55:48 +0300

    First commit

---
+++ app/src/main/java/com/uludag/can/spacexdroid/home/MainActivity.java
@@ -0,0 +1,36 @@
+package com.uludag.can.spacexdroid.home;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.uludag.can.spacexdroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+                switch (item.getItemId()) {
+                    case R.id.navigation_rockets:
+                        return true;
+                    case R.id.navigation_launches:
+                        return true;
+                    case R.id.navigation_upcomings:
+                        return true;
+                }
+                return false;
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
