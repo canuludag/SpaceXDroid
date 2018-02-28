5ed4c44159de1ec49913bfa2f1efddd594045f06
Can Uludag <cucanuludag@gmail.com>
2018-02-28 19:21:42 +0300

    feat: Implement model classes with Moshi

--- app/src/main/java/com/uludag/can/spacexdroid/models/Launch.java
+++ app/src/main/java/com/uludag/can/spacexdroid/models/Launch.java
@@ -1,4 +1,20 @@
 package com.uludag.can.spacexdroid.models;
 
+import com.squareup.moshi.Json;
+
 public class Launch {
+
+    @Json(name = "launch_year")
+    public final int launchYear;
+    @Json(name = "launch_success")
+    public final boolean success;
+    public final String details;
+    public final Rocket rocket;
+
+    public Launch(int launchYear, boolean success, String details, Rocket rocket) {
+        this.launchYear = launchYear;
+        this.success = success;
+        this.details = details;
+        this.rocket = rocket;
+    }
 }
--- app/src/main/java/com/uludag/can/spacexdroid/models/Rocket.java
+++ app/src/main/java/com/uludag/can/spacexdroid/models/Rocket.java
@@ -1,4 +1,15 @@
 package com.uludag.can.spacexdroid.models;
 
 public class Rocket {
+    public final String id;
+    public final String name;
+    public final String country;
+    public final String description;
+
+    public Rocket(String id, String name, String country, String description) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.description = description;
+    }
 }
