a183253d43da1af51d371710d7179f85d3112cbd
Can Uludag <cucanuludag@gmail.com>
2018-02-28 19:41:21 +0300

    feat: Add ViewModelModule to Dagger graph

--- app/src/main/java/com/uludag/can/spacexdroid/base/ApplicationComponent.java
+++ app/src/main/java/com/uludag/can/spacexdroid/base/ApplicationComponent.java
@@ -4,6 +4,7 @@ import com.uludag.can.spacexdroid.home.launches.LaunchesFragment;
 import com.uludag.can.spacexdroid.home.rockets.RocketsFragment;
 import com.uludag.can.spacexdroid.home.upcomings.UpcomingsFragment;
 import com.uludag.can.spacexdroid.networking.ApiModule;
+import com.uludag.can.spacexdroid.viewmodel.ViewModelModule;
 
 import javax.inject.Singleton;
 
@@ -12,6 +13,7 @@ import dagger.Component;
 @Singleton
 @Component(modules = {
         ApiModule.class,
+        ViewModelModule.class
 })
 public interface ApplicationComponent {
 
---
+++ app/src/main/java/com/uludag/can/spacexdroid/viewmodel/ViewModelModule.java
@@ -0,0 +1,18 @@
+package com.uludag.can.spacexdroid.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.uludag.can.spacexdroid.home.rockets.RocketsViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(RocketsViewModel.class)
+    abstract ViewModel bindRocketsViewModel(RocketsViewModel viewModel);
+}
